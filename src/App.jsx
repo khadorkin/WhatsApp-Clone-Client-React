0df8c8f6a3029baedb219c64c28afddc753258bd
DAB0mB <emanor6@gmail.com>
2019-03-15 16:15:30 +0800

    step-by-step-final@next: Initial version of the full tutorial

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import * as React from 'react'
+import ChatsListScreen from './components/ChatsListScreen'
+
+const App = () => (
+  <div>
+    <ChatsListScreen />
+  </div>
+)
+
+export default App
